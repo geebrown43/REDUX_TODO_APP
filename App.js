0ee8ee3a2193f0f3dcc82a39c7fb4317204a39c0
Ellis Mwekunda <Scareb15@gmail.com>
2018-03-21 15:50:29 -0600

    will add Provider from react-redux

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import Todo from './src/components/Todo'
+
+
+
+
+const App = () => (
+  
+    <Todo />
+ 
+)
+
+
+export default App
