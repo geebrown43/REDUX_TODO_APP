e376833551beee8dae051d7b62bb4f0bcf841d2e
Ellis Mwekunda <Scareb15@gmail.com>
2018-03-21 15:50:51 -0600

    added ACTION types

---
+++ src/actions/types.js
@@ -0,0 +1,27 @@
+let id = 0;
+export const ADD_ITEM = text => {
+    return {
+        type: "ADD",
+        completed: false,
+        style: 'none',
+        id: id++,
+        text
+    }
+}
+export const SHOW_LINE = id => {
+  return {
+    type: "COMPLETE",
+    completed: true,
+    id,
+    style: 'line-through'
+  };
+};
+
+export const HIDE_LINE = id => {
+    return {
+      type: "NOT_COMPLETE",
+      completed: false,
+      id,
+      style: 'none'
+    }
+}
